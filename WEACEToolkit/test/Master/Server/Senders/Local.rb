ce279d17a8fac2b2f933d6e62321e6c1f8a2d242
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2010-01-06 18:50:46 +0000

    * Regression: Added test cases for Senders/Local.
    git-svn-id: https://weacemethod.svn.sourceforge.net/svnroot/weacemethod@99 5f6fdd18-d50c-4247-9f77-f36982294e7a

--- WEACEToolkit/test/Master/Server/Senders/Local.rb
+++ WEACEToolkit/test/Master/Server/Senders/Local.rb
@@ -66,6 +66,174 @@ module WEACE
             end
           end
 
+          # Test a normal run with 1 action to execute with parameters
+          def test1ActionParameters
+            executeSender(
+              'DummyUser',
+              {
+                'DummyTool' => [
+                  [ 'DummyActionWithParams', [ 'Param1', 'Param2' ] ]
+                ]
+              },
+              :DummySlaveClient => true,
+              :ClientAddRegressionActions => true,
+              :ClientInstallActions => [
+                [ 'DummyProduct', 'DummyTool', 'DummyActionWithParams' ]
+              ],
+              :ClientConfigureProducts => [
+                [
+                  'DummyProduct', 'DummyTool',
+                  {}
+                ]
+              ]
+            ) do |iError|
+              assert($Variables[:SlaveActions] != nil)
+              assert_equal('DummyUser', $Variables[:SlaveActions][:UserID])
+              lActions = $Variables[:SlaveActions][:ActionsToExecute]
+              assert_equal(
+                {
+                  'DummyTool' => {
+                    'DummyActionWithParams' => [
+                      [ 'Param1', 'Param2' ]
+                    ]
+                  }
+                },
+                lActions
+              )
+            end
+          end
+
+          # Test a normal run with 1 action to execute 2 times with different parameters
+          def test1ActionTwiceDifferentParameters
+            executeSender(
+              'DummyUser',
+              {
+                'DummyTool' => [
+                  [ 'DummyActionWithParams', [ 'Param11', 'Param21' ] ],
+                  [ 'DummyActionWithParams', [ 'Param12', 'Param22' ] ]
+                ]
+              },
+              :DummySlaveClient => true,
+              :ClientAddRegressionActions => true,
+              :ClientInstallActions => [
+                [ 'DummyProduct', 'DummyTool', 'DummyActionWithParams' ]
+              ],
+              :ClientConfigureProducts => [
+                [
+                  'DummyProduct', 'DummyTool',
+                  {}
+                ]
+              ]
+            ) do |iError|
+              assert($Variables[:SlaveActions] != nil)
+              assert_equal('DummyUser', $Variables[:SlaveActions][:UserID])
+              lActions = $Variables[:SlaveActions][:ActionsToExecute]
+              assert_equal(
+                {
+                  'DummyTool' => {
+                    'DummyActionWithParams' => [
+                      [ 'Param11', 'Param21' ],
+                      [ 'Param12', 'Param22' ]
+                    ]
+                  }
+                },
+                lActions
+              )
+            end
+          end
+
+          # Test a normal run with 2 different actions to execute
+          def test2Actions
+            executeSender(
+              'DummyUser',
+              {
+                'DummyTool' => [
+                  [ 'DummyActionWithParams', [ 'Param1', 'Param2' ] ],
+                  [ 'DummyAction', [] ]
+                ]
+              },
+              :DummySlaveClient => true,
+              :ClientAddRegressionActions => true,
+              :ClientInstallActions => [
+                [ 'DummyProduct', 'DummyTool', 'DummyAction' ],
+                [ 'DummyProduct', 'DummyTool', 'DummyActionWithParams' ]
+              ],
+              :ClientConfigureProducts => [
+                [
+                  'DummyProduct', 'DummyTool',
+                  {}
+                ]
+              ]
+            ) do |iError|
+              assert($Variables[:SlaveActions] != nil)
+              assert_equal('DummyUser', $Variables[:SlaveActions][:UserID])
+              lActions = $Variables[:SlaveActions][:ActionsToExecute]
+              assert_equal(
+                {
+                  'DummyTool' => {
+                    'DummyActionWithParams' => [
+                      [ 'Param1', 'Param2' ]
+                    ],
+                    'DummyAction' => [
+                      []
+                    ]
+                  }
+                },
+                lActions
+              )
+            end
+          end
+
+          # Test a normal run with 2 different actions to execute on 2 different Tools
+          def test2ActionsDifferentTools
+            executeSender(
+              'DummyUser',
+              {
+                'DummyTool' => [
+                  [ 'DummyAction', [] ]
+                ],
+                'DummyTool2' => [
+                  [ 'DummyAction2', [] ]
+                ]
+              },
+              :DummySlaveClient => true,
+              :ClientAddRegressionActions => true,
+              :ClientInstallActions => [
+                [ 'DummyProduct', 'DummyTool', 'DummyAction' ],
+                [ 'DummyProduct', 'DummyTool2', 'DummyAction2' ]
+              ],
+              :ClientConfigureProducts => [
+                [
+                  'DummyProduct', 'DummyTool',
+                  {}
+                ],
+                [
+                  'DummyProduct', 'DummyTool2',
+                  {}
+                ]
+              ]
+            ) do |iError|
+              assert($Variables[:SlaveActions] != nil)
+              assert_equal('DummyUser', $Variables[:SlaveActions][:UserID])
+              lActions = $Variables[:SlaveActions][:ActionsToExecute]
+              assert_equal(
+                {
+                  'DummyTool' => {
+                    'DummyAction' => [
+                      []
+                    ]
+                  },
+                  'DummyTool2' => {
+                    'DummyAction2' => [
+                      []
+                    ]
+                  }
+                },
+                lActions
+              )
+            end
+          end
+
         end
 
       end
