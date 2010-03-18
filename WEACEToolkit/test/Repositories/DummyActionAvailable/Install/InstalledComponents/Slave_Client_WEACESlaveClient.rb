5cc042430e11ba8e6b3be3e8ce072a63c197bf28
Muriel Salvan <MurielSalvan@users.sourceforge.net>
2010-03-18 14:17:15 +0000

    * Created a Volatile directory for plugins to put data that will be volatile (such as queues...). * MasterServer is now architectured around a queuing mechanism to send SlaveActions to SlaveClients. * MasterServer uses the Volatile directory to store SlaveClients queues (regression updated). * Added --send to MasterServer command line options to process waiting SlaveActions to be sent (regression updated). * Files to be transfered by MasterServer are kept until all SlaveActions needing the file are sent. * Regression: MasterServer regression now executes using Dummy/MasterServerInstalled repository by default instead of Empty. * Regression: Removed useless repositories.
    git-svn-id: https://weacemethod.svn.sourceforge.net/svnroot/weacemethod@195 5f6fdd18-d50c-4247-9f77-f36982294e7a

--- WEACEToolkit/test/Repositories/DummyActionAvailable/Install/InstalledComponents/Slave_Client_WEACESlaveClient.rb
+++
@@ -1,7 +0,0 @@
-
-{
-  :InstallationDate => '2009-12-08 19:06:55',
-  :Description => 'The WEACE Slave Client.',
-  :Author => 'murielsalvan@users.sourceforge.net',
-  :InstallationParameters => '--provider DummySlaveProvider'
-}
